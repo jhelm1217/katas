f45f75907c7e5b43d24cc97fe780afda7148fd5b
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-09-15 19:01:35 -0400

    new kata

---
+++ Sept11/greet.js
@@ -0,0 +1,10 @@
+function greet (name, owner) {
+    // Add code here
+    return name === owner ? "Hello boss" : "Hello guest";
+  }
+  
+  
+  
+  // Parameters- there are two, name and owner.
+  // It should either say boss or guest. 
+  // if the name is the same as the owner then it will say boss, otherwise it will say guest. 
