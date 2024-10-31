be7c2ed2888ef170a87ca5286343f09ffe5dbf4e
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-10-31 19:56:37 -0400

    week of oct29

---
+++ Oct29/age.js
@@ -0,0 +1,9 @@
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :) 
+      return parseInt(inputString[0]);
+    }
+    
+    // we want to create a program where it returns the girls age 0-9
+    // the first character will always be a number. 
+    // inputString[0] extracts the first character of the string.
+    // parseInt converts this character to an integer.
