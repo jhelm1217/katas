a35fd637ac8ea51c22832d1629f6af5a15b4aa9f
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-12-18 06:44:23 -0500

    kata 3

---
+++ Dec17/alarm.js
@@ -0,0 +1,7 @@
+function setAlarm(employed, vacation) {
+    return employed && !vacation;
+}
+
+
+// use logical && and ! for when they are on vacation or working/employed. 
+// Return true if employed and not on vacation, otherwise return false
